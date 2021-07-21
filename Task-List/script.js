9ff99390484ea4a7fd25c2d561ea578bab4130bc
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-07-21 17:32:12 -0300

    Adicionando limite de tasks

--- Task-List/script.js
+++ Task-List/script.js
@@ -1,12 +1,19 @@
-function saveNote() {
+var qtdNotes = 0
+
+function saveNote(qtdNotes) {
     
     let note = document.getElementById('note').value 
     let outputNote = document.getElementById('container')
     let savedNote = document.createElement('p')
     
-    document.getElementById('note').value = ''
-
-    savedNote.textContent = `${note}`
-
-    outputNote.appendChild(savedNote)
+    if(this.qtdNotes < 6) {
+        document.getElementById('note').value = ''
+        
+        savedNote.textContent = `${note}`
+        
+        outputNote.appendChild(savedNote)
+        
+        this.qtdNotes++
+    } else
+        window.alert('Notes limit reached.')
 }
