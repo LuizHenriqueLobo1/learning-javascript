cff269a303c50d04cdef64a10e2d98e067464b5e
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-07-28 22:05:13 -0300

    Adicionando material de funções

---
+++ Introducao-JavaScript/funcoes.js
@@ -0,0 +1,21 @@
+// declarando função sem parâmetros e retorno
+function printaHello() {
+    console.log('Hello, world')
+}
+
+printaHello()
+
+// declarando função com parâmetros e retorno
+function soma(a, b) {
+    return a + b
+}
+
+console.log(soma(3, 2))
+
+// declarando função sem parâmetros e com retorno
+function atribui() {
+    return 10
+}
+
+let a = atribui()
+console.log(a)
