ee76f9c3c984dcbb7e82b4c624e45ab07bd199ec
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-08-10 18:08:40 -0300

    Adicionando função de remover task

--- Task-List/script.js
+++ Task-List/script.js
@@ -1,6 +1,7 @@
 let newTask   = document.getElementById('task')
-let placeTask = document.querySelector('.container')
+let placeTask = document.querySelector('.content')
 let addTask   = document.getElementById('add')
+var id = 1
 
 newTask.addEventListener('keypress', function(f) {
     if(f.key == "Enter")
@@ -11,12 +12,28 @@ addTask.addEventListener('click', function(f) {
     generateTask()
 })
 
-function generateTask() {
+function generateTask(id) {
     let field = document.createElement('div')
     let task = document.createElement('p')
+    let btn = document.createElement('input')
+
     field.setAttribute('class', 'new-task')
+    field.setAttribute('id', `task${this.id}`)
+    btn.setAttribute('class', 'delete')
+    btn.setAttribute('type', 'button')
+    btn.setAttribute('value', 'x')
+    btn.setAttribute('onclick', 'deleteTask(`${this.parentNode.id}`)')
+
     task.textContent = `${newTask.value}`
     field.appendChild(task)
+
+    field.appendChild(btn)
     placeTask.appendChild(field)
+
     document.getElementById('task').value = ''
+    this.id++
+}
+
+function deleteTask(trashTask) {
+    document.getElementById(trashTask).remove()
 }
