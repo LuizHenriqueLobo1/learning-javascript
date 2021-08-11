64852802837c0b359c8acd5aa9c493db88e8127f
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-08-11 15:58:38 -0300

    Adicionando verificação de tamanho da task

--- Task-List/script.js
+++ Task-List/script.js
@@ -13,25 +13,28 @@ addTask.addEventListener('click', function(f) {
 })
 
 function generateTask(id) {
-    let field = document.createElement('div')
-    let task = document.createElement('p')
-    let btn = document.createElement('input')
 
-    field.setAttribute('class', 'new-task')
-    field.setAttribute('id', `task${this.id}`)
-    btn.setAttribute('class', 'delete')
-    btn.setAttribute('type', 'button')
-    btn.setAttribute('value', 'x')
-    btn.setAttribute('onclick', 'deleteTask(`${this.parentNode.id}`)')
+    if(newTask.value.length >= 3) {
+        let field = document.createElement('div')
+        let task = document.createElement('p')
+        let btn = document.createElement('input')
 
-    task.textContent = `${newTask.value}`
-    field.appendChild(task)
+        field.setAttribute('class', 'new-task')
+        field.setAttribute('id', `task${this.id}`)
+        btn.setAttribute('class', 'delete')
+        btn.setAttribute('type', 'button')
+        btn.setAttribute('value', 'x')
+        btn.setAttribute('onclick', 'deleteTask(`${this.parentNode.id}`)')
 
-    field.appendChild(btn)
-    placeTask.appendChild(field)
+        task.textContent = `${newTask.value}`
+        field.appendChild(task)
 
-    document.getElementById('task').value = ''
-    this.id++
+        field.appendChild(btn)
+        placeTask.appendChild(field)
+
+        document.getElementById('task').value = ''
+        this.id++
+    }
 }
 
 function deleteTask(trashTask) {
