49b10e95ab55afa9a4305de5e5e1d2c294e54120
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-08-12 15:57:02 -0300

    Adicionando projeto Random-Color-Pallete-Generator

---
+++ Random-Color-Pallete-Generator/js/main.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-action').addEventListener('click', () => {
+
+    for(let i = 0; i < 5; i++) {
+        let color    = document.getElementById(`color-${i+1}`)
+        let txtColor = document.getElementById(`txt-color-${i+1}`)
+        let newColor = generateColor()
+        color.style.backgroundColor = `${newColor}`
+        txtColor.textContent = `${newColor}`
+    } 
+})
+
+function generateColor() {
+
+    const chars = '0123456789ABCDEF'
+    let color   = '#'
+
+    for(let i = 0; i < 6; i++) {
+        color += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+    return color;
+}
