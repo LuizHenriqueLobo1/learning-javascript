fb4df72ef2e192cefc8d9e66108b4df826dc7df8
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-09-03 11:56:01 -0300

    Adicionando conteúdo sobre forEach e map

--- Introducao-JavaScript/vetores.js
+++ Introducao-JavaScript/vetores.js
@@ -18,12 +18,27 @@ let tamanho = vetor.length // atribuindo tamanho do vetor a uma variável
 
 console.log(`Tamanho do vetor: ${tamanho}`)
 
+// percorrendo vetor de forma tradicional
 console.log(`Printando valores do vetor com for tradicional: `)
 for(var i = 0; i < tamanho; i++) {
 	console.log(`${vetor[i]}`)
 }
 
+// percorrendo vetor usando for in
 console.log(`Printando valores do vetor com for usando in: `)
 for(let i in vetor) {
 	console.log(`${vetor[i]}`)
 }
+
+// percorrendo vetor usando forEach
+vetor.forEach((item, posicao) => {
+	console.log(`${item}[${posicao}]`)
+})
+
+// criando vetor secundario com os tipos dos elementos do vetor primario
+// usando map
+let vetorTipos = vetor.map(item => {
+	return typeof(item)
+})
+
+console.log(vetorTipos)
