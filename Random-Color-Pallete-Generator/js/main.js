28ad2ea21c7af97dab3a2dee9c148c3efb5351a4
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-02-26 23:37:50 -0300

    Adicionando feature de bloquear cor na paleta

--- Random-Color-Pallete-Generator/js/main.js
+++ Random-Color-Pallete-Generator/js/main.js
@@ -1,29 +1,47 @@
-document.getElementById('btn-action').addEventListener('click', () => {
+let colors = Array(5).fill('#FFFFFF');
+let savedColors = [];
 
-    for(let i = 0; i < 5; i++) {
-        let color    = document.getElementById(`color-${i+1}`)
-        let txtColor = document.getElementById(`txt-color-${i+1}`)
-        let newColor = generateColor()
-        color.style.backgroundColor = `${newColor}`
-        txtColor.value = `${newColor}`
-    } 
-})
+const renderColors = () => {
+    colors.forEach((color, index) => {
+        document.querySelector(`#color-${index+1}`).style.backgroundColor = color;
+        document.querySelector(`#txt-color-${index+1}`).value = color;
+    });
+}
+
+document.querySelector('#btn-action').onclick = () => {
+    setRandomColorsArray();
+    renderColors();
+}
 
 function generateColor() {
-
     const chars = '0123456789ABCDEF'
     let color   = '#'
-
     for(let i = 0; i < 6; i++) {
         color += chars.charAt(Math.floor(Math.random() * chars.length))
     }
-
     return color;
 }
 
-function copyColor(num) {
+function setRandomColorsArray() {
+    colors = colors.map(color => color = generateColor());
+    if (savedColors.length) savedColors.forEach(savedColor => colors[savedColor.index] = savedColor.color);
+}
 
+function copyColor(num) {
     let myColor = document.getElementById(`txt-color-${num}`)
     myColor.select()
     document.execCommand('copy')
+}
+
+function lockColor(index) {
+    const savedColor = { index: index, color: colors[index] };
+    const savedColorIndex = savedColors.findIndex(sc => 
+        sc.index === savedColor.index && sc.color === savedColor.color);
+    if(savedColorIndex === -1) {
+        savedColors.push(savedColor);
+        document.querySelector(`#lock-icon-${index+1}`).textContent = 'lock';
+    } else {
+        savedColors.splice(savedColorIndex, 1);
+        document.querySelector(`#lock-icon-${index+1}`).textContent = 'lock_open';
+    }
 }
