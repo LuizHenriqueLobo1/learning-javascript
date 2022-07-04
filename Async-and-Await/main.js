eddc02332f8ffb695da9a68ef6f236aae61dd33c
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-07-04 19:25:35 -0300

    Atualizando projeto Async-and-Await

--- Async-and-Await/main.js
+++ Async-and-Await/main.js
@@ -1,3 +1,5 @@
+const div = document.querySelector('#container');
+
 let consumeJson = async _ => {
     const response = await fetch('data.json');
     const json = await response.json();
@@ -5,5 +7,5 @@ let consumeJson = async _ => {
 }
 
 consumeJson()
-    .then(response => console.log(response))
+    .then(response => div.textContent = JSON.stringify(response))
     .catch(error => console.log(error));
